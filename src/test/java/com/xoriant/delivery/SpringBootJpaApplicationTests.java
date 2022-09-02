18907accf6a57fa580d952086c511502da71a63a
Harshawardhan Patil <89831742+Harshpatil7770@users.noreply.github.com>
2022-09-02 10:38:29 +0530

    mockito used

--- src/test/java/com/xoriant/delivery/SpringBootJpaApplicationTests.java
+++ src/test/java/com/xoriant/delivery/SpringBootJpaApplicationTests.java
@@ -3,11 +3,16 @@ package com.xoriant.delivery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SpringBootJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@BeforEach
+	public void setup(){
+
 	}
 
 }
